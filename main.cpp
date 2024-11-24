1904f45965881de34e297155520774e1327d9027
ammonia2 <m500.abdullah@gmail.com>
2024-11-24 11:28:33 +0500

    Class Structure model

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// g++ main.cpp -o app -lsfml-graphics -lsfml-window -lsfml-system && ./app
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// CLASSES STRUCTURE:
+//  class Traffic Signal: (not all nodes have traffic signals)
+//      green time, yellow 3, red: total (can be assumed 60) - green - yellow
+
+//  class Intersection (Node):
+//      vector incoming roads
+//      vector outgoing roads
+//      traffic Signal
+
+//  class Road:
+//      length, congestion level
+//      vector vehicles
+
+//  class Vehicle:
+//      road, position on road
+//      progress of route
+//      start, End Intersection
+
+//  class Emergency Vehicle (inherits from Vehicle):
+//      different priority, dynamic routing (next road will be calculated at every intersection)
+
+//  class TrafficManagement:
+//      list of intersections,
+//      list of roads, vehicles,
+//      roads and vehicles stored in an adjacency list
+//      clock for updating vehicles
